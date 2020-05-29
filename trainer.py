91cf8539080f6204a30a38201303ffa5155c0a5c
andrewmcdonald27 <andrewmcdonald27@gmail.com>
2020-05-29 11:11:00 -0700

    update trainer.py; failing with dtype issue when loading initial hyp

--- trainer.py
+++ trainer.py
@@ -7,8 +7,8 @@ def train_sfgp(name):
     labels = ['mu_sf', 's^2_sf', 'L_sf', 'noise_sf']
 
     # 2) reshape training data from CSV to valid format
-    X = data[:, 0:2].reshape(-1, 2)  # columns 1 and 2 are (x,y) points
-    y = data[:, 2].reshape(-1, 1)  # column 3 is f(x,y)
+    X = sifi[:, 1:3].reshape(-1, 2)  # columns 1 and 2 are (x,y) points (0 is index)
+    y = sifi[:, 3].reshape(-1, 1)  # column 3 is f(x,y)
 
     # 3) initialize model
     len_sf = 0.1
@@ -33,10 +33,10 @@ def train_mfgp(name):
     labels = ['mu_lo', 's^2_lo', 'L_lo', 'mu_hi', 's^2_hi', 'L_hi', 'rho', 'noise_lo', 'noise_hi']
 
     # 2) reshape training data from CSV to valid format
-    X_L = lofi[:, 0:2].reshape(-1, 2)  # columns 1 and 2 are (x,y) points
-    y_L = lofi[:, 2].reshape(-1, 1)  # column 3 is f(x,y)
-    X_H = hifi[:, 0:2].reshape(-1, 2)
-    y_H = hifi[:, 2].reshape(-1, 1)
+    X_L = lofi[:, 1:3].reshape(-1, 2)  # columns 1 and 2 are (x,y) points (0 is index)
+    y_L = lofi[:, 3].reshape(-1, 1)  # column 3 is f(x,y)
+    X_H = hifi[:, 1:3].reshape(-1, 2)
+    y_H = hifi[:, 3].reshape(-1, 1)
 
     # 3) initialize model
     len_L = 0.2
